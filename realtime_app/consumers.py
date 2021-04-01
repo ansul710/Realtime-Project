82b087e37d6483be134acbb1537152d0b371c406
ansul kotadia <ansul71098@gmail.com>
2021-04-01 20:00:23 +0530

    changes

--- realtime_app/consumers.py
+++ realtime_app/consumers.py
@@ -12,7 +12,7 @@ class WSConsumer(WebsocketConsumer):
         for i in range(2000):
             # self.send(json.dumps({'message': '33'}))
             # self.send(json.dumps({'message': 'HelloWorld22222222'}))
-            self.send(json.dumps({'message': randint(1, 100)}))
+            self.send(json.dumps({'message': randint(1, 1000)}))
             sleep(1)
         self.disconnect(self)
 
